f3536e6670ce68578fdce90b65ae171737952a4a
Dayvid Santana <santana.dayvid@outlook.com>
2022-01-16 17:22:13 -0300

    Categories, routes to DB

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose') // Import do mongoose
+require('../models/Categoria')  // Chamando arquivo do model
+const Categoria = mongoose.model('categorias') // Passando referencia do model para a variavel
 
 
 router.get('/',(req, res)=>{
@@ -12,7 +15,24 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res)=>{
-    res.send('Página de categorias')
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res)=>{
+    res.render('admin/addcategorias')
+})
+
+router.post('/categorias/add', (req,res)=>{
+    const novaCategoria = { // Recebe os dados do formulario
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log('Categoria salva com sucesso')
+    }).catch((err)=>{
+        console.log('Erro ao savar categoria'+err)
+    })
 })
 
 router.get('/testes', (req, res)=>{
