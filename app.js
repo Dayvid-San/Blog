893e656cb30d0999a6fd9be11d71b8e7d1cc83fc
Dayvid Santana <santana.dayvid@outlook.com>
2022-01-17 18:03:13 -0300

    add middleware

--- app.js
+++ app.js
@@ -6,27 +6,49 @@ const mongoose = require('mongoose');
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path');
+const session = require('express-session')
+const flash = require('connect-flash')
 
 
 // Configurações
+    // Sessão
+    app.use(session({
+        secret: "CursoNode",  // Chave para gerar sessão (ponha uma segura)
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    // Middleware
+    app.use((req, res, next)=>{
+        res.locale.success_msg = req.flash("success_msg") // São variaveis globais
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+
     // Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
     
     // Handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars');
+    //app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    //app.set('view engine', 'handlebars');
 
     // Mongoose
     mongoose.Promise = global.Promise
     mongoose.connect('mongodb://localhost/blogapp').then(() => {
-        console.log('O banco está conctado')
+        console.log('O banco está conectado')
     }).catch((err)=>{
         console.log('Não conectado ao mongo. Algo está errado. => '+err)
     })
 
     // Public
     app.use(express.static(path.join(__dirname, 'public')))
+
+    app.use((req, res, next)=>{
+        console.log('Nova requisição')
+        next()
+    })
 
 // Rotas
 app.use('/user', admin)
