2dfc0598a66975185061105988390edb17227491
Dayvid Santana <santana.dayvid@outlook.com>
2022-02-08 09:22:22 -0300

    delete postagens

--- routes/admin.js
+++ routes/admin.js
@@ -188,4 +188,16 @@ router.post('/postagem/edit', (req,res)=>{
     })
 })
 
+
+router.get('/postagens/deletar/:id',(req,res)=>{
+    Postagem.remove({_id: req.params.id}).then(()=>{
+        req.flash('sucess_msg', 'postagem deletada com sucesso!')
+        res.redirect('/admin/postagens')
+    }).catch((err)=>{
+        req.flash('error_msg', 'Houve um erro interno')
+        res.redirect('/admin/postagens')
+    })
+})
+
+
 module.exports = router
