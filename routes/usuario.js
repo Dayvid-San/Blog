70e5a8a19427d24f47254bd687557daae194b034
Dayvid Santana <santana.dayvid@outlook.com>
2022-02-22 23:24:37 -0300

    cadastro usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('../models/Usuario')
 const Usuario = mongoose.model('usuarios')
+const bcrypt = require('bcryptjs')
 
 
 router.get('/register', (req,res)=>{
@@ -55,6 +56,67 @@ router.post('/registro', (req,res)=>{
     }
     else{
         
+        //Checando se o email cadastrado já existe
+        Usuario.findOne({email: req.body.email})
+        .then(usuario => {
+
+            if(usuario){
+                req.flash('error_msg', 'Já existe uma conta com esse e-mail no nosso sistema.')
+                res.redirect('/ususarios/registro')
+
+            }
+            else{
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+
+                // Envez de salvarmos, nós encriptamos a senha
+                bcrypt.genSalt(10, (err, salt) => {
+
+                    // PARAMETROS
+                    // 1: Qual valor vai "hashear"
+                    // 2: o "Sal"
+                    // 3: O callback
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+
+                        if(erro){
+                            req.flash('erro_msg', 'Houve um erro durante o salvamento do usuário.')
+                            res.redirect('/')
+                            
+                        }
+
+                        // Hashando
+                        novoUsuario.senha = hash
+
+                        // Agora sim! Estamos salvando
+                        novoUsuario.save()
+                        .then(() => {
+
+                            req.flash('success_msg', 'Usuário criado com sucesso!')
+                            req.redirect('/')
+
+                        })
+                        .catch((err) => {
+
+                            res.flash('err_msg', 'Teve um erro ao criar o usuário. Tente novamente!')
+                            req.redirect('/')
+
+                        })
+
+                    })
+                })
+                
+
+            }
+        })
+        .catch((err) => {
+            req.flash('error_msg', 'Houve um erro interno')
+            res.redirect('/')
+        })
     }
 })
 
